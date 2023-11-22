bd9dccb41333210f3862ebfc804863ce865de5ad
Ughinetor <u.figus@studenti.unisa.it>
2023-11-22 18:12:59 +0100

    (US2) Rule Interface

---
+++ src/localIFTTT/Rule.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package localIFTTT;
+
+public interface Rule {
+    //Check the Trigger of the Rule
+    boolean checkRule();
+    
+    //Active the Action of the Rule
+    void activateRule();
+}
