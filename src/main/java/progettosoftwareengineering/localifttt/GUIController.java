a7faeb0ce70767ec58a27c39655b58c116b599a9
Ilguardianonotturno <tanatome08012001@gmail.com>
2023-11-26 13:16:52 +0100

    (US3) TableView implementation

--- src/main/java/progettosoftwareengineering/localifttt/GUIController.java
+++ src/main/java/progettosoftwareengineering/localifttt/GUIController.java
@@ -7,7 +7,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
 public class GUIController implements Initializable{
@@ -20,10 +23,28 @@ public class GUIController implements Initializable{
     private TextField insertRuleName;
     @FXML
     private Button saveButton;
+    @FXML
+    private TableView<Rule> ruleTable;
+    @FXML
+    private TableColumn<Rule, String> nameColumn;
+//    TODO when a concrete trigger will be implemented
+//    @FXML
+//    private TableColumn<Rule, Trigger> triggerColumn;  
+//    TODO when a concrete action will be implemented
+//    @FXML
+//    private TableColumn<Rule, Action> actionColumn;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         saveButton.disableProperty().bind(Bindings.isEmpty(insertRuleName.textProperty()));
+        
+        nameColumn.setCellValueFactory(new PropertyValueFactory("name"));
+//        add when a concrete trigger will be implemented
+//        triggerColumn.setCellValueFactory(new PropertyValueFactory("trigger"));
+
+//        add when a concrete action will be implemented
+//        actionColumn.setCellValueFactory(new PropertyValueFactory("action"));
+        ruleTable.setItems(RuleCollection.getInstance().getRules());
     }
     
     @FXML
