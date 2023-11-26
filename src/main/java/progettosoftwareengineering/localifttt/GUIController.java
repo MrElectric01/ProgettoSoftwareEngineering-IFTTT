d2904d75975660dbbeaeafbf522b71bad744cc87
MrElectric01/Christian+Conato <christianconato123@gmail.com>
2023-11-26 13:11:21 +0100

    (US2) addRulePane implementation

--- src/main/java/progettosoftwareengineering/localifttt/GUIController.java
+++ src/main/java/progettosoftwareengineering/localifttt/GUIController.java
@@ -2,26 +2,54 @@ package progettosoftwareengineering.localifttt;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class GUIController implements Initializable{
 
     @FXML
     private VBox homePane;
+    @FXML
+    private VBox addRulePane;
+    @FXML
+    private TextField insertRuleName;
+    @FXML
+    private Button saveButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO
+        saveButton.disableProperty().bind(Bindings.isEmpty(insertRuleName.textProperty()));
     }
     
     @FXML
-    //Handle the 'Add Rule' button fire (change interface).
-    private void handleAddRule(ActionEvent event) {
-        // TODO
+    private void addRule(ActionEvent event) {
+        homePane.setVisible(false);
+        addRulePane.setVisible(true);
     }
 
+    @FXML
+    private void handleCancel(ActionEvent event) {
+        fromAddToHomePane();
+    }
+
+    @FXML
+    private void handleSave(ActionEvent event) {
+        RuleCollection.getInstance().addRule(new Rule(insertRuleName.getText(), null, null));
+        fromAddToHomePane();
+    }
     
+    private void fromAddToHomePane() {
+        clearAllFields();
+        addRulePane.setVisible(false);
+        homePane.setVisible(true);
+    }
+
+    private void clearAllFields() {
+        insertRuleName.clear();
+    }
 }
